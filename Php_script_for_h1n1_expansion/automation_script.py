ba46b5d797c0a7cd16e6ac6010322558a75fe7d7
FrancescoPenasa <francesco.penasa-1@studenti.unitn.it>
2020-12-02 18:19:45 +0100

    h1n1 expansion extracted

--- Php_script_for_h1n1_expansion/automation_script.py
+++ Php_script_for_h1n1_expansion/automation_script.py
@@ -1,8 +1,15 @@
 # imports
 import os 
 
+os.system("cp ../Data/h1n1_expansion ./ -r")
 
 # read result files
 for file in os.listdir("h1n1_expansion/"):
 	os.system("php anno-hsf5.php h1n1_expansion/" + file)
 	print(file)
+
+os.system("mkdir results")
+os.system("mv *.csv results/")
+os.system("mv results/anno-hsf5.csv anno-hsf5.csv")
+os.system("rm h1n1_Hs_.csv")
+print(os.system("ls -l results | wc -l"))
